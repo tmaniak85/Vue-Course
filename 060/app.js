6ac830cbc3a4ad92ff2871df22372e75d96195d1
tmaniak85 <tomasz.maniak.85@gmail.com>
2023-10-01 14:46:18 +0200

    Vue: Behind the Scenes

---
+++ 060/app.js
@@ -0,0 +1,79 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            currentUserInput: '',
+            message: 'Vue is great!',
+        };
+    },
+    methods: {
+        // saveInput(event) {
+        //     this.currentUserInput = event.target.value;
+        // },
+        setText() {
+            // this.message = this.currentUserInput;
+            this.message = this.$refs.userText.value;
+        },
+    },
+    beforeCreate() {
+        console.log('beforeCreate()');
+    },
+    created() {
+        console.log('created()');
+    },
+    beforeMount() {
+        console.log('beforeMount');
+    },
+    mounted() {
+        console.log('mounted()');
+    },
+    beforeUpdate() {
+        console.log('beforeUpdate()');
+    },
+    updated() {
+        console.log('updated()');
+    },
+    beforeUnmount() {
+        console.log('beforeUnmount()');
+    },
+    unmounted() {
+        console.log('unmounted()');
+    }
+});
+
+app.mount('#app');
+
+setTimeout(() => {
+    app.unmount();
+}, 10000);
+
+const app2 = Vue.createApp({
+    data() {
+        return {
+            favoriteMeal: 'Pizza'
+        }
+    }
+})
+
+app2.mount('#app2');
+
+// --------------------------reactivity only with js
+
+const data = {
+    message: 'Hello!',
+    longMessage: 'Hello! World!'
+};
+
+const handler = {
+    set(target, key, value) {
+        if (key === 'message') {
+            target.longMessage = value + ' World!';
+        }
+        target.message = value;
+    }
+};
+
+const proxy = new Proxy(data, handler);
+
+proxy.message = 'Hello!!!!';
+
+console.log(proxy.longMessage);
